12a47293c6907ec2634d3e8bc00f0bd6e8fccd92
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-23 19:25:55 -0700

    reformat js, debugging in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,41 @@
-$(document).ready(function() {
-  $("form#Beeps").submit(function(event) {
-    event.preventDefault();
-    const userInput = $("#userInput").val("");
-    let beepBoopArr = beepBoop(userInput);
-    const array = toString(beepBoopArr);
+// Business Logic
+function beepBoop(userInput) {
+  const beepBoopArr = [];
+  for(let i = 0; i <= userInput; i++){
+    if((i + " ").indexOf(1) < 0) {
+      beepBoopArr.push(i);
+      } else {
+        beepBoopArr.push("Beep!");
+        };
+    if((i + " ").indexOf(2) < 0) {
+      //Dont do anything
+    } else {
+      beepBoopArr.splice(i, 1, "Boop!");
+      }; 
+    if((i + " ").indexOf(3) < 0) {
+        //Dont do anything
+      } else {
+        beepBoopArr.splice(i, 1, "Wont you be my neighbor?");
+        };
+    }; console.log(beepBoopArr);
+    return beepBoopArr;
+}  
 
-    if(parseInt(userInput) < 100) {
-      $(".array").text(array);
-    } else{
-      $(".array").text("whoa, whoa whoa... thats too many for this neighbor")
-    }
+
+$(document).ready(function() {
+  $("#Beeps").submit(function(event) {
+    event.preventDefault();
+    const userInput = $("input#input").val();
+    const beepBoopArr = beepBoop(userInput);
     
 
+    if(userInput < 100) {
+      $(".returner").show();
+      $("#array").show(beepBoopArr);
+    } else{
+      $(".returner").append("whoa, whoa whoa... thats too many for this neighbor")
+    }
+    $(".returner").show()
+    
   });
 
-
-// Business Logic
-    function beepBoop(userInput) {
-      const beepBoopArr = [];
-      for(let i = 0; i <= userInput; i++){
-        if((i + " ").indexOf(1) < 0) {
-          beepBoopArr.push(i);
-          } else {
-            beepBoopArr.push("Beep!");
-            };
-        if((i + " ").indexOf(2) < 0) {
-          //Dont do anything
-        } else {
-          beepBoopArr.splice(i, 1, "Boop!");
-          }; 
-        if((i + " ").indexOf(3) < 0) {
-            //Dont do anything
-          } else {
-            beepBoopArr.splice(i, 1, "Wont you be my neighbor?");
-            };
-        }; console.log(beepBoopArr);
-        return beepBoopArr;
-        }  
